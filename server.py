1d8d405d0d6193796ac56c92a5bbcabdab1b6661
nocaBriS_PC <markuspetzke@gmail.com>
2020-06-21 21:20:49 +0200

    "Test Branche created"

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, Response, request
 import os
+from PIL import Image
 
 IMAGE_FOLDER = os.path.join('static' , 'images')
 
@@ -11,40 +12,26 @@ app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
 def index():
     return render_template('index.html')
 
-def gen():
-    cap = cv2.VideoCapture('today.mp4')
+def gen(data):
 
-    # Read until video is completed
-    while(cap.isOpened()):
-      # Capture frame-by-frame
-        ret, img = cap.read()
-        if ret == True:
-            img = cv2.resize(img, (0,0), fx=0.5, fy=0.5) 
-            frame = cv2.imencode('.jpg', img)[1].tobytes()
-            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            time.sleep(0.1)
-        else: 
-            break
+    image = Image.frombytes('RGBA', (640,480), data)
+    image.save("test.jpg")
         
-
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/get_stream', methods=['POST'])
 def get_stream():
-    image = request.files['image_data']
-    print(image.filename)
-    image.save(os.path.join('C:/Users/marku/Desktop/Projekt X/git/RESTful/static/images/' + image.filename))
-    return "image: succed" + image.filename
+    data = request.data
 
-    print(request.files['image_data'])
+
+    return Response(gen(data),
+                mimetype='multipart/x-mixed-replace; boundary=frame') 
 
 @app.route('/')
 @app.route('/test_image')
 def test_image():
+   
+   
+
     while True:
         full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'image.jpg')
         return render_template("test.html", user_image = full_filename)
