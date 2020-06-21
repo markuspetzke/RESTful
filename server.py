27bda4e457f6726a2ee5e8e4678041d1ef1fee79
nocaBriS_PC <markuspetzke@gmail.com>
2020-06-21 13:28:25 +0200

    Bilder speichern/senden

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, Response, request
+<<<<<<< HEAD
+import os
+=======
 
+>>>>>>> 0d531d2d32b6d1d53ac36c43f35ce6251b991b6c
 
 app = Flask(__name__)
 
@@ -31,7 +35,14 @@ def video_feed():
 
 @app.route('/get_stream', methods=['POST'])
 def get_stream():
+<<<<<<< HEAD
+    image = request.files['image_data']
+    print(image.filename)
+    image.save(os.path.join(app.root_path, 'images/' + image.filename))
+    return "image: succed" + image.filename
+=======
     print(request.files['image_data'])
+>>>>>>> 0d531d2d32b6d1d53ac36c43f35ce6251b991b6c
 
 if __name__ == '__main__':
     # defining server ip address and port
