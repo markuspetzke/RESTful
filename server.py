5f3baf18ed72dd4c81f388d0b7a1cda32772293f
nocaBriS_PC <markuspetzke@gmail.com>
2020-06-21 11:49:47 +0200

    Server test

---
+++ server.py
@@ -0,0 +1,35 @@
+from flask import FLASK, render_template, Response, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen():
+    cap = cv2.VideoCapture('today.mp4')
+
+    # Read until video is completed
+    while(cap.isOpened()):
+      # Capture frame-by-frame
+        ret, img = cap.read()
+        if ret == True:
+            img = cv2.resize(img, (0,0), fx=0.5, fy=0.5) 
+            frame = cv2.imencode('.jpg', img)[1].tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            time.sleep(0.1)
+        else: 
+            break
+        
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/get_stream', methods=['POST'])
+def get_stream():
+    print(request.files['image_data'])
+
