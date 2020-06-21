9d7c87f21c60b4e4cdb6078e78c8e5c06f7e64d1
nocaBriS_PI <markuspetzke@gmail.com>
2020-06-21 16:04:50 +0100

    Camera Update

--- camera.py
+++ camera.py
@@ -1,5 +1,5 @@
 import request, requests,  json, time
-import cv2
+import cv2, os
 
 cam = cv2.VideoCapture(0)
 time.sleep(0.2)
@@ -9,16 +9,16 @@ while True:
     if not ret:
         print("Failed to grab frame")
         break
-
-    image = cv2.imwrite("image.jpg", frame)
-
-    with open(image, "rb") as imageFile:
+    path = os.getcwd() + '/images/'
+    success = cv2.imwrite('/home/pi/gitServer/RESTful/images/image.jpg', frame)
+    print success
+    with open('/home/pi/gitServer/RESTful/images/image.jpg', "rb") as imageFile:
         # f = imageFile.read()
         # b = bytearray(f)    
         url = 'http://192.168.8.10:5000/get_stream'
         header = {'Content-Type': 'application/octet-stream'}
         try:
-            response = requests.post(url, files=[('image_data',('test.jpg', imageFile, 'image/jpg'))])
+            response = requests.post(url, files=[('image_data',('image.jpg', imageFile, 'image/jpg'))])
             print(response.status_code)
         except Exception as e:
             print(e)
